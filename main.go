5918baba9c16e8fbc17a1a8c9867d1d26a044844
Dorival Pedroso <dorival.pedroso@gmail.com>
2020-09-12 01:36:37 +0000

    Fix launch.json

--- main.go
+++ main.go
@@ -3,5 +3,8 @@ package main
 import "gosl/io"
 
 func main() {
-	io.Pf("Hello using Gosl\n")
+	io.Pf("Hello Gosl (6031)!!!\n")
+	for i, n := range []int{6, 0, 3, 1} {
+		io.Pf("%d: the number is now = %d\n", i, n)
+	}
 }
